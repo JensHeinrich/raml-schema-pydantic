c247fa7aefb03affc969bf838c89ba514b36e8fd
Jens Heinrich <github.com/JensHeinrich>
2023-06-01 13:07:15 +0200

    Tweak typing

--- src/raml_schema_pydantic/type_expression/_util.py
+++ src/raml_schema_pydantic/type_expression/_util.py
@@ -2,34 +2,39 @@ from __future__ import annotations
 
 from collections import UserString
 from typing import Any
+from typing import List
 from typing import Sequence
+from typing import Type
 from typing import TypeGuard
+from typing import TypeVar
 
 from ._shunt import Operator
+from ._shunt.token_types import Token
 
+_T = TypeVar("_T")
 
 # Operators
-OPERATOR_UNION = Operator(
-    symbol="|",
+OPERATOR_UNION: Operator[Token] = Operator(
+    symbol=Token("|"),
     name="Union",
 )
-OPERATOR_ARRAY = Operator(
-    symbol="[]",
+OPERATOR_ARRAY: Operator[Token] = Operator(
+    symbol=Token("[]"),
     name="Array",
     precedence=5,
     unary=True,
     unary_position="postfix",
 )
-OPERATOR_NOOP = Operator(
-    symbol="NOOP",
+OPERATOR_NOOP: Operator[Token] = Operator(
+    symbol=Token("NOOP"),
     name="NOOP",
     unary=True,
 )
 
-OPS = [OPERATOR_ARRAY, OPERATOR_UNION]
+OPS: List[Operator[Token]] = [OPERATOR_ARRAY, OPERATOR_UNION]
 
 
-def is_iterable_of(val: Sequence[Any], t: _T) -> TypeGuard[Sequence[_T]]:
+def is_iterable_of(val: Sequence[Any], t: Type[_T]) -> TypeGuard[Sequence[_T]]:
     """Check all values of a list against the type.
 
     Args:
@@ -39,7 +44,8 @@ def is_iterable_of(val: Sequence[Any], t: _T) -> TypeGuard[Sequence[_T]]:
     Returns:
         TypeGuard[Iterable[_T]]: TypeGuard
     """
-    assert not isinstance(val, (str, UserString))
+    if isinstance(val, (str, UserString)):
+        return False
     _bool = all(isinstance(x, t) for x in val)
     print(
         f"{val} {'is' if _bool else 'is not'} a sequence of {t}. (Type is {type(val)}) containing ({list(type(v) for v in val)})"
