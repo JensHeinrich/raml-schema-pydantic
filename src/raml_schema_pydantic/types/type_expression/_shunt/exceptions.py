7295f031befc30dadb98517a89c4aa810617495b
Jens Heinrich <github.com/JensHeinrich>
2023-07-10 10:35:52 +0200

    Create additional exceptions.
    TODO: Add RPNToken

--- src/raml_schema_pydantic/types/type_expression/_shunt/exceptions.py
+++ src/raml_schema_pydantic/types/type_expression/_shunt/exceptions.py
@@ -1,12 +1,52 @@
 """Custom exceptions."""
 from typing import TYPE_CHECKING
 
+from pydantic import PydanticValueError
+
 
 if TYPE_CHECKING:
     from .token_types import ClosingDelim
-    from .token_types import Operator
+    from .token_types import RPNToken, Token
 
-NonMatchingDelimitersException = ValueError("The delimiters need to be matched.")
+
+class NonMatchingDelimitersException(ValueError):
+    """Exception for a delimiter closed by an unexpected delimiter."""
+
+    msg = "The delimiters need to be matched."
+
+
+class NonMatchingPlaceholderCount(PydanticValueError):
+    """Exception for a mismatch between the expected and the provided number of placeholders."""
+
+    msg_template = "wrong placeholder count {actual_count}, expected {expected_count}"
+
+    def __init__(self, *, actual_count: int, expected_count: int) -> None:
+        """Initialize exception.
+
+        Args:
+            actual_count (int): Expected number of placeholders.
+            expected_count (int): Provided number of placeholders.
+        """
+        super().__init__(actual_count=actual_count, expected_count=expected_count)
+
+
+# TODO Replace all those lists with tuples
+class ListLengthError(PydanticValueError):
+    """Exception for a list of unexpected length."""
+
+    code = "list.length"
+    msg_template = "wrong list length {actual_length}, expected {expected_length}"
+
+    def __init__(self, *, actual_length: int, expected_length: int) -> None:
+        """Initialize exception.
+
+        Args:
+            actual_length (int): Length of the provided list.
+            expected_length (int): Expected length of the list.
+        """
+        super().__init__(actual_length=actual_length, expected_length=expected_length)
+
+
 UnexpectedValueException = ValueError("Unexpected value")
 UnusedTokensException = ValueError("Unused tokens")
 CaseNotImplementedException = NotImplementedError(
@@ -20,10 +60,40 @@ class InvalidTokenException(TypeError):
     pass
 
 
+class UnaryPrefixOperatorExpected(InvalidTokenException):
+    """Exception for wrong operator type."""
+
+    def __init__(self, token: "Token"):
+        """Initialize exception.
+
+        Args:
+            input_data (str): string being parsed.
+            op (Token): invalid operator.
+        """
+        super().__init__(
+            f"Expected an unary prefix operator. but got unary postfix or binary operator {token}."
+        )
+
+
+class BinaryOrUnaryPostfixOperatorExpected(InvalidTokenException):
+    """Exception for wrong operator type."""
+
+    def __init__(self, token: "Token"):
+        """Initialize exception.
+
+        Args:
+            input_data (str): string being parsed.
+            op (Token): invalid operator.
+        """
+        super().__init__(
+            f"Expected an unary postfix or an binary operator, but got unary prefix operator {token}."
+        )
+
+
 class StartsWithNonPrefixUnaryOperatorException(InvalidTokenException):
     """Exception for data starting with a unary postfix operator."""
 
-    def __init__(self, input_data: str, op: "Operator"):
+    def __init__(self, input_data: str, op: "RPNToken"):
         """Initialize exception.
 
         Args:
@@ -51,10 +121,14 @@ class StartsWithClosingDelimiterException(InvalidTokenException):
 
 
 __all__ = (
+    "BinaryOrUnaryPostfixOperatorExpected",
+    "CaseNotImplementedException",
+    "ListLengthError",
     "NonMatchingDelimitersException",
+    "NonMatchingPlaceholderCount",
+    "StartsWithClosingDelimiterException",
+    "StartsWithNonPrefixUnaryOperatorException",
+    "UnaryPrefixOperatorExpected",
     "UnexpectedValueException",
     "UnusedTokensException",
-    "CaseNotImplementedException",
-    "StartsWithNonPrefixUnaryOperatorException",
-    "StartsWithClosingDelimiterException",
 )
